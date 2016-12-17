64967c6c041a551e370fe910e582e32d16fdd88b
André Puel <andrepuel@gmail.com>
2016-12-16 22:41:45 -0200

    Skeleton of Typescript with Browserify

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var ts = require('gulp-typescript');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+
+gulp.task("typescripts", function() {
+    return gulp.src("**.ts")
+           .pipe(ts({}))
+           .pipe(gulp.dest('./ts'));
+});
+
+gulp.task("default", ["typescripts"], function() {
+    return browserify()
+        .add('ts/index.js')
+        .bundle()
+        .on('error', function (error) { throw error; })
+        .pipe(source('./dist/index.js'))
+        .pipe(gulp.dest('./'));
+});
